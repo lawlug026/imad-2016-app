796f2e7421a67c1f15288dfeab58e8acd3af1aeb
lawlug026 <ujwalgoyal.2@gmail.com>
2016-09-28 15:51:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ var maintext=document.getElementById("mainBody");
 //Counter Button
 var counterc=0;
 var counterButton=document.getElementById('counter');
-counterButton.onClick= function(){
+counterButton.onClick = function(){
     //Make a request to counter endpoint
 
     
