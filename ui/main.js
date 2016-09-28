c51edcd9d83b04c327c757db80a5f14dc6b4402d
lawlug026 <ujwalgoyal.2@gmail.com>
2016-09-28 14:14:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,6 @@ var moveRight=function(){marginLeft+=1; img.style.marginLeft=marginLeft+"px";};
 var img=document.getElementById('madi');
 img.onclick=function(){var interval=setInterval(moveRight, 20);
 };
+
+var maintext=document.getElementById("mainBody");
+maintext.innerHTML="New Text";
