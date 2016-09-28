45fca6ead3764dabe0071eed05e3940c5d03f7b9
lawlug026 <ujwalgoyal.2@gmail.com>
2016-09-28 13:48:10 +0530

    move the madi

--- server.js
+++ server.js
@@ -9,25 +9,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/webpage1', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'webpage1.html')); 
-});
 
-app.get('/webpage3', function (req, res){
-   res.send("this is article three & no high level coding is done"); 
-});
-
-app.get('/webpage2', function (req, res){
-   res.send("this is article two & no high level coding is done"); 
-});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
 console.log('Loaded!');
+
+var marginLeft=0;
+var moveRight=function(){marginLeft+=1; img.style.marginLeft=marginLeft+"px";};
+var img=document.getElementById('madi');
+img.onclick=function(){var interval=setInterval(moveRight, 20);
+};
