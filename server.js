831cccc71211fe161fed843ccaf26981f7771909
lawlug026 <ujwalgoyal.2@gmail.com>
2016-09-28 20:37:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ var names=[];
 app.get('/submit-name/:name', function(req, res){
     //get the current name from the request
     
-    var name=req.params.name;//1000 extract the name value
+    var name=req.query.name;//1000 extract the name value
     names.push(name);
     //JSON=Javascript Object Notation
     res.send(JSON.stringify(names));
