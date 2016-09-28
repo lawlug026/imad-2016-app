9308a97a83892fe21e1c4dc5bd6b3cc101512486
lawlug026 <ujwalgoyal.2@gmail.com>
2016-09-28 15:31:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,7 @@
 console.log('Loaded!');
+/*
 
+//Moving the image
 var marginLeft=0;
 var moveRight=function(){marginLeft+=1; img.style.marginLeft=marginLeft+"px";};
 var img=document.getElementById('madi');
@@ -7,3 +9,20 @@ img.onclick=function(){var interval=setInterval(moveRight, 20);
 };
 
 var maintext=document.getElementById("mainBody");
+*/
+
+
+//Counter Button
+var counterButton=document.getElementById("counter");
+counterButton.onClick= function(){
+    //Make a request to counter endpoint
+
+    
+    //Capture the response & store it in a variable
+
+    
+    //render the variable to the current span
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter.toString();
+};
