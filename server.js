2162695f7c762b6fb4509f728e3428082afb8578
lawlug026 <ujwalgoyal.2@gmail.com>
2016-10-02 01:42:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,8 +15,22 @@ app.get('/counter', function(req, res){
 });
 
 app.get('/ui/style.css', function (req, res) {
+  res.send("This my article one");
+});
+
+//additional url's
+app.get('/ui/article1.html', function (req, res) {
+  res.send("This my article one");
+});
+
+app.get('/ui/article2.html', function (req, res) {
+  res.send("This my article one");
+});
+
+app.get('/ui/article3.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
